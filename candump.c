a334b0c12ee68ab0fc6ecba5d45a81d3f09c2cf1
Kazuyoshi Kamitsukasa <jou4.kz@gmail.com>
2025-12-26 10:44:58 +0900

    add file output option into candump

--- candump.c
+++ candump.c
@@ -13,6 +13,7 @@ void print_usage_candump(char *arg0, char *arg1)
 	fprintf(stderr, "Options:\n");
 	fprintf(stderr, "  -t <type>                      (timestamp: (a)bsolute/(d)elta - default 'a')\n");
 	fprintf(stderr, "  -v                             (verbose CAN flags)\n");
+	fprintf(stderr, "  -O <outfile>                   (logfile to write)\n");
 	fprintf(stderr, "\n");
 	fprintf(stderr, "Format of <channel>: <channel-num>{f|F}{_}{b|B<bitrate>}{d|D<data-bitrate>}\n");
 	fprintf(stderr, "  examples:\n");
@@ -121,6 +122,11 @@ typedef struct {
 } output_thread_param;
 
 typedef struct {
+	FILE *fp;
+	int verbose;
+} file_output_thread_param;
+
+typedef struct {
 	HANDLE thread_handle;
 	DWORD thread_id;
 } thread;
@@ -129,6 +135,7 @@ thread channel_threads[MAX_CHANNELS];
 
 #define MAX_QUEUE_SIZE 10000
 log_queue log_q;
+log_queue file_q;
 
 DWORD WINAPI channel_thread(LPVOID param) {
     long id;
@@ -174,6 +181,8 @@ DWORD WINAPI output_thread(LPVOID param) {
 		if (dequeue_frame(&log_q, &log) == 0) {
 			adjust_timestamp(&log, tp->timestamp_type, tp->start_time);
 			fprint_log(stdout, &log, tp->verbose);
+			// enqueue to file output
+			enqueue_frame(&file_q, &log);
 		}else{
 			Sleep(1);
 		}
@@ -184,19 +193,125 @@ DWORD WINAPI output_thread(LPVOID param) {
 	while (dequeue_frame(&log_q, &log) == 0){
 		adjust_timestamp(&log, tp->timestamp_type, tp->start_time);
 		fprint_log(stdout, &log, tp->verbose);
+		// enqueue to file output
+		enqueue_frame(&file_q, &log);
 	}
 
 	return 0;
 }
 
+// Buffer for sort
+#define FILE_OUTPUT_BUFFER_SIZE 1000
+typedef struct {
+	FILE* fp;
+    can_log* buffer;
+    int count;
+    int capacity;
+	int verbose;
+} file_output_buffer;
+
+int compare_by_timestamp(const void* a, const void* b) {
+    const can_log* log_a = (const can_log*)a;
+    const can_log* log_b = (const can_log*)b;
+
+    if (log_a->timestamp < log_b->timestamp) {
+        return -1;
+    } else if (log_a->timestamp > log_b->timestamp) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void flush_output_buffer(file_output_buffer* buf, int force_flush) {
+    int i;
+
+    if (buf->count == 0) {
+        return;
+    }
+
+    int frames_to_output = buf->count;
+    if (!force_flush && frames_to_output > 1) {
+        frames_to_output = buf->count - 1;
+    }
+
+    if (frames_to_output <= 0) {
+        return;
+    }
+
+    qsort(buf->buffer, frames_to_output, sizeof(can_log), compare_by_timestamp);
+
+    for (i = 0; i < frames_to_output; i++) {
+		fprint_log(buf->fp, &buf->buffer[i], buf->verbose);
+    }
+    fflush(buf->fp);
+
+	// Move remainings to top
+    if (!force_flush && buf->count > frames_to_output) {
+        memmove(&buf->buffer[0],
+                &buf->buffer[frames_to_output],
+                sizeof(can_log) * (buf->count - frames_to_output));
+        buf->count = buf->count - frames_to_output;
+    } else {
+        buf->count = 0;
+    }
+}
+
+void append_output_buffer(file_output_buffer* buf, can_log* log){
+	if (buf->count < buf->capacity) {
+		memcpy(&buf->buffer[buf->count], log, sizeof(can_log));
+		buf->count++;
+	} else {
+		flush_output_buffer(buf, 1);
+		memcpy(&buf->buffer[0], log, sizeof(can_log));
+		buf->count = 1;
+	}
+}
+
+DWORD WINAPI file_output_thread(LPVOID param) {
+	can_log log;
+	file_output_thread_param *tp = (file_output_thread_param *)param;
+	file_output_buffer buf;
+
+	// initialize buffer
+	buf.fp = tp->fp;
+	buf.verbose = tp->verbose;
+	buf.buffer = (can_log*)malloc(sizeof(can_log) * FILE_OUTPUT_BUFFER_SIZE);
+    buf.count = 0;
+    buf.capacity = FILE_OUTPUT_BUFFER_SIZE;
+
+	while(!stop_flag){
+		if (dequeue_frame(&file_q, &log) == 0) {
+			append_output_buffer(&buf, &log);
+		}else{
+			Sleep(1);
+		}
+	}
+
+	// output all log before exiting
+	while (dequeue_frame(&file_q, &log) == 0){
+		append_output_buffer(&buf, &log);
+	}
+	flush_output_buffer(&buf, 1);
+
+	free(buf.buffer);
+
+	return 0;
+}
+
 int candump(int argc, char *argv[]){
-	int i, channel_num, verbose;
+	int i, channel_num, verbose, file_output;
 	char timestamp_type;
 	can_channel ch;
+	char *filepath;
+	FILE *outfile;
 	uint64_t start_time;
 	output_thread_param output_tp;
 	HANDLE output_thread_handle;
 	DWORD output_thread_id;
+	file_output_thread_param file_output_tp;
+	HANDLE file_output_thread_handle;
+	DWORD file_output_thread_id;
 
 	if(argc <= 2){
 		print_usage_candump(argv[0], argv[1]);
@@ -205,6 +320,7 @@ int candump(int argc, char *argv[]){
 
 	timestamp_type = 'a';
 	verbose = 0;
+	file_output = 0;
 
 	kv_initialize();
 	memset(channel_threads, '\0', sizeof(thread) * MAX_CHANNELS);
@@ -228,6 +344,17 @@ int candump(int argc, char *argv[]){
 				print_usage_candump(argv[0], argv[1]);
 				return EXIT_FAILURE;
 			}
+		}
+		else if(strcmp(argv[i], "-O") == 0){
+			if(i + 1 >= argc){
+				fprintf(stderr, "Error: Missing outfile value after %s\n\n", argv[i]);
+				print_usage_candump(argv[0], argv[1]);
+				return EXIT_FAILURE;
+			}
+
+			i++;
+			filepath = argv[i];
+			file_output = 1;
 		}
 		else if(strcmp(argv[i], "help") == 0){
 			print_usage_candump(argv[0], argv[1]);
@@ -253,6 +380,12 @@ int candump(int argc, char *argv[]){
         return 1;
     }
 
+    if (init_queue(&file_q, MAX_QUEUE_SIZE) != 0) {
+        fprintf(stderr, "Failed to initialize frame queue for file output\n");
+        kv_cleanup_channels();
+        return 1;
+    }
+
 	start_time = get_unix_time();
 	kv_sync_bus_on();
 
@@ -274,6 +407,32 @@ int candump(int argc, char *argv[]){
         stop_flag = 1;
 		goto err;
     }
+
+	if (file_output) {
+		// Run file output thread
+		file_output_tp.verbose = verbose;
+		if(fopen_s(&outfile, filepath, "w") != 0){
+			fprintf(stderr, "cannot open: %s\n", filepath);
+        	stop_flag = 1;
+			goto err;
+		}
+		file_output_tp.fp = outfile;
+		file_output_thread_handle = CreateThread(
+			NULL,
+			0,
+			file_output_thread,
+			&file_output_tp,
+			0,
+			&file_output_thread_id
+		);
+
+		if (file_output_thread_handle == NULL) {
+			fprintf(stderr, "Failed to create file output thread\n");
+			stop_flag = 1;
+			goto err;
+		}
+
+	}
 
 	// Run channel threads
 	for(i = 0; i < MAX_CHANNELS; i++){
@@ -305,15 +464,29 @@ int candump(int argc, char *argv[]){
 	}
 
 	CloseHandle(output_thread_handle);
+    destroy_queue(&log_q);
+
+	if (file_output) {
+		// wait until exiting
+    	WaitForSingleObject(file_output_thread_handle, INFINITE);
+		// close threads
+		CloseHandle(file_output_thread_handle);
+    	destroy_queue(&file_q);
+		// close files
+		fclose(outfile);
+	}
 
     kv_cleanup_channels();
-    destroy_queue(&log_q);
 
 	return EXIT_SUCCESS;
 
 err:
     kv_cleanup_channels();
     destroy_queue(&log_q);
+	if (file_output) {
+    	destroy_queue(&file_q);
+		fclose(outfile);
+	}
 
 	return EXIT_FAILURE;
 }
